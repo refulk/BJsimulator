a4d7c067e2d3429b90bcb41432239c002395964e
refulk <pradarenan@gmail.com>
2019-01-24 09:05:16 -0200

    estrutura inicial

---
+++ BJ_simulator/src/Simulator/Baralho.java
@@ -0,0 +1,26 @@
+package Simulator;
+
+import java.util.List;
+
+/*
+public class Baralho {
+	private List<Carta> cartasBaralho;
+	
+	public Baralho(List<Carta> cartasBaralho) {
+		this.cartasBaralho = cartasBaralho;
+	}
+
+	public Baralho retiraCarta (int idCliente) {
+
+		for (Cliente cliente : clientesDoBanco) {
+			if(cliente.getId() == idCliente)
+				return cliente;
+		}
+		return null;
+	}
+	
+
+}
+
+*/
+
