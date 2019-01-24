90c58738d1ece9f6bcf79c3c01d8c3318bd7bb59
refulk <pradarenan@gmail.com>
2019-01-24 11:25:00 -0200

    Gerando Baralho sem embaralhar

--- BJ_simulator/src/Simulator/Baralho.java
+++ BJ_simulator/src/Simulator/Baralho.java
@@ -1,26 +1,81 @@
 package Simulator;
 
-import java.util.List;
+import java.util.Random;
 
-/*
+
 public class Baralho {
-	private List<Carta> cartasBaralho;
 	
-	public Baralho(List<Carta> cartasBaralho) {
-		this.cartasBaralho = cartasBaralho;
+	int qtdTotal;
+	Carta [] baralho;
+	
+	public Baralho()
+	{
+		this.qtdTotal = 416;
+		baralho = new Carta[qtdTotal];
 	}
-
-	public Baralho retiraCarta (int idCliente) {
-
-		for (Cliente cliente : clientesDoBanco) {
-			if(cliente.getId() == idCliente)
-				return cliente;
+	
+	public static void main(String[] args) {
+		Baralho b1 = new Baralho();		
+		b1.iniciaBaralho();
+		b1.exibeBaralho();
+		System.out.println(b1.retiraCarta());
+		b1.exibeBaralho();
+	}
+	
+	public void iniciaBaralho() {		
+		int [] faces = {1,2,3,4,5,6,7,8,9,10,10,10,10};
+		int tamBaralho = faces.length;
+		int qtdCarta = qtdTotal / faces.length;
+		int [] qtd = new int[tamBaralho];
+		int face;
+		int i = 0;		
+		
+		for(i = 0; i < tamBaralho; i++)
+		{
+			qtd[i] = qtdCarta;
 		}
-		return null;
+		i = 0;
+		
+		while(i< qtdTotal)
+		{
+//			face = new Random().nextInt(faces.length);
+			face = i % 13;			
+			if(qtd[face] > 0)
+			{
+				baralho[i] = new Carta();
+				baralho[i].setValor(faces[face]);
+				i++;
+				qtd[face]--;
+			}
+		}
 	}
 	
-
+	public void exibeBaralho()
+	{
+		int i = 0;
+		while(i < qtdTotal)
+		{
+			System.out.println(i + " - " + baralho[i].getValor());
+			i++;
+		}
+	}	
+	
+	public int retiraCarta()
+	{
+		int carta = new Random().nextInt(baralho.length);
+		int cartaRetorno = baralho[carta].getValor();
+		qtdTotal--;
+		for(int i = carta; i < qtdTotal; i++)
+		{
+//			baralho[i] = baralho[i+1];
+			baralho[i].setValor(baralho[i+1].getValor());
+		}
+		System.out.println(carta);
+		return cartaRetorno;
+	}
 }
 
-*/
+
+
+
 
--- BJ_simulator/src/Simulator/Carta.java
+++ BJ_simulator/src/Simulator/Carta.java
@@ -1,12 +1,11 @@
 package Simulator;
 
 public class Carta {
-	
 	private int valor;
 	
-	public Carta(int valor)
+	public Carta()
 	{
-		this.valor = valor;
+		this.valor = 0;
 	}
 	
 	public void setValor(int valor)
@@ -18,76 +17,7 @@ public class Carta {
 	{
 		return valor;
 	}
-}
-
-
-
-/*
-
-import java.util.Random;
- 
-public class ArrayBidimensional{
- 
-public static void main (String [] args){
- 
-String [] faces = new String[]
-{"As", "2", "3", "4", "5", "6", "7", "8", "9", "10", 
-"Valete", "Dama", "Rei"};
- 
-String [] nipes = new String[]
-{"Espadas", "Paus", "Copas", "Ouros"};
- 
-////////////////////////// Determinando a carta
-System.out.println("ESCOLHA DETERMINADA - 
-A carta escolhida sera " + faces[8] + " de " + nipes[2]);
- 
-////////////////////////// ESCOLHA ALEATORIA - RANDOM
-Random r = new Random();
-System.out.println("ESCOLHA ALEATORIA 01 ( Índice = "
-+ r.nextInt(10) + " )"); //numero máximo - (entre 0 e 10)
- 
-//////// escolha aleatoria de um indice
-int i = r.nextInt(faces.length);
-//////// usando o indice para escolher a face
-String face = faces[i];
- 
-/////// fixando so o nipe
-System.out.println("ESCOLHA ALEATORIA 02 ( " + face + " de "
-+ nipes[0] + " )");
- 
-int j = r.nextInt(nipes.length);
-String nipe = nipes[j];
- 
-////// totalmente aleatorio
-System.out.println("ESCOLHA ALEATORIA 03 ( " + face + " de "
-+ nipe + " )");
- 
-}
- 
-}
-
-
-
-public class ClasseTeste {
-	
-	int numero;
-	String cliente;
-	double saldo;
-	
-	public static void main(String[] args) {
-		ClasseTeste teste = new ClasseTeste();
-		
-		teste.numero = 999;
-		teste.cliente = "Jose";
-		teste.saldo = 1000;
-		
-		System.out.println(teste.numero);
-		System.out.println(teste.cliente);
-		System.out.println(teste.saldo);
-	}
 
 }
 
 
-
-*/
