b045eba5a500a5a3b7bc695ed4cf9aaa73446d38
refulk <pradarenan@gmail.com>
2019-01-25 09:25:14 -0200

    Dia2-inicioRegrasJogada

--- BJ_simulator/src/Simulator/Jogada.java
+++ BJ_simulator/src/Simulator/Jogada.java
@@ -28,9 +28,72 @@ public class Jogada {
 		{
 			b.geraEmbaralhado();			
 		}
-		m1.setValor(b.distribuiCarta());
+
+		m1.setValor(b.distribuiCarta()); //carta visivel
 		j1.setValor(b.distribuiCarta());
+		m2.setValor(b.distribuiCarta()); //carta escondida
 		j2.setValor(b.distribuiCarta());
-		m2.setValor(b.distribuiCarta());
+		
+		realizaJogada();
+		
 	}
+	
+	private void realizaJogada()
+	{
+		int lj1, lj2, lm1;
+		lj1 = j1.getValor();
+		lj2 = j2.getValor();
+		lm1 = m1.getValor();
+		
+		if(lj1 == lj2)//tem par
+		{
+			if(
+			(lj1 == 1)//Par de Ás
+			|| ((lj1 == 2 || lj1 == 3) && (lm1 >= 2 && lm1 <= 7)) //2 ou 3, mesa 2 a 7, #divida.
+			|| ((lj1 == 4 || lj1 == 7 || lj1 == 8) && (lm1 >=4 && lm1 <=6)) //4, 7 ou 8, mesa de 4 a 6, #divida.
+			|| ((lj1 == 9) && (lm1 >=2 && lm1 <=6)) //9, mesa de 2 a 6, #divida.			
+			) //final do IF
+			{
+				//Dividir
+				//jogadaDividir(lj1);
+				//jogadaDividir(lj2);
+			}
+			else
+			{
+				
+			}
+		}
+		else //não tem par
+		{
+			
+		}
+		
+	}
+	
+	private void jogadaDividir(int carta)
+	{
+		//quando o jogador dividir
+		j2.setValor(b.distribuiCarta());
+		if(j1.getValor() == 1 || j2.getValor() == 1)
+		{
+			jogadaAs();
+		}
+		else
+		{
+			jogadaSoma();
+		}
+	}
+	
+	private void jogadaSoma() //SemPar
+	{
+		
+	}
+	
+	private void jogadaAs() //tem 1 As
+	{
+		int lj2 = j2.getValor();
+		
+		if()
+	}
+	
 }
